1f761e4349b703501f3d44123fe17a6108f684e5
DenisDragos1 <dragosdenisdanut@yahoo.com>
2024-01-13 15:37:45 +0200

    first commit

---
+++ frontend/src/components/common/RoomTypeSelector.jsx
@@ -0,0 +1,88 @@
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";  // Adaugă această linie pentru a folosi PropTypes
+import { getRoomTypes } from "../utils/ApiFunctions";
+
+const RoomTypeSelector = ({ handleRoomInputChange, newRoom }) => {
+  const [roomTypes, setRoomTypes] = useState([""]);
+  const [showNewRoomTypeInput, setShowNewRoomTypeInput] = useState(false);
+  const [newRoomType, setNewRoomType] = useState("");
+
+  useEffect(() => {
+    getRoomTypes().then((data) => {
+      setRoomTypes(data);
+    });
+  }, []);
+
+  const handleNewRoomTypeInputChange = (e) => {
+    setNewRoomType(e.target.value);
+  };
+
+  const handleAddNewRoomType = () => {
+    if (newRoomType !== "") {
+      setRoomTypes([...roomTypes, newRoomType]);
+      setNewRoomType("");
+      setShowNewRoomTypeInput(false);
+    }
+  };
+
+  return (
+    <>
+      {roomTypes.length > 0 && (
+        <div>
+          <select
+            required
+            className="form-select"
+            name="roomType"
+            onChange={(e) => {
+              if (e.target.value === "Add New") {
+                setShowNewRoomTypeInput(true);
+              } else {
+                handleRoomInputChange(e);
+              }
+            }}
+            value={newRoom.roomType}
+          >
+            <option value="">Select a room type</option>
+            <option value={"Add New"}>Add New</option>
+            {roomTypes.map((type, index) => (
+              <option key={index} value={type}>
+                {type}
+              </option>
+            ))}
+          </select>
+          {showNewRoomTypeInput && (
+            <div className="mt-2">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter New Room Type"
+                  value={newRoomType}
+                  onChange={handleNewRoomTypeInputChange}
+                />
+                <button
+                  className="btn btn-hotel"
+                  type="button"
+                  onClick={handleAddNewRoomType}
+                >
+                  Add
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
+
+// Adaugă validarea pentru props folosind PropTypes
+RoomTypeSelector.propTypes = {
+  handleRoomInputChange: PropTypes.func.isRequired,
+  newRoom: PropTypes.shape({
+    roomType: PropTypes.string.isRequired,
+    // Alte proprietăți ale obiectului `newRoom` pot fi adăugate aici
+  }).isRequired,
+};
+
+export default RoomTypeSelector;
---
+++ frontend/src/components/layout/MainHeader.jsx
@@ -0,0 +1,16 @@
+
+const MainHeader = () => {
+	return (
+		<header className="header-banner">
+			<div className="overlay"></div>
+			<div className="animated-texts overlay-content">
+				<h1>
+					Welcome to <span className="hotel-color"> RoamNest Hotel</span>
+				</h1>
+				
+			</div>
+		</header>
+	)
+}
+
+export default MainHeader
