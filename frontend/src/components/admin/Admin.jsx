96a69b35117a28e038bb753e67156bf6016f9611
DenisDragos1 <dragosdenisdanut@yahoo.com>
2024-01-14 11:03:38 +0200

    admin

--- frontend/src/components/admin/Admin.jsx
+++ frontend/src/components/admin/Admin.jsx
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom"
 const Admin = () => {
 	return (
 		<section className="container mt-5">
-			<h2>Welcome to Adimin Panel</h2>
+			<h2>Welcome to Adimin Dashboard</h2>
 			<hr />
 			<Link to={"/existing-rooms"}>Manage Rooms</Link> <br />
 			<Link to={"/existing-bookings"}>Manage Bookings</Link>
+
 		</section>
 	)
 }
